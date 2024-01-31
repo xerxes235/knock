23fde52b4a056b207b05660add2a3d060aaf9ee8
xerxes235 <xerxes235@gmail.com>
2024-01-31 16:41:24 +0330

    fix

--- src/knockd.c
+++ src/knockd.c
@@ -1805,7 +1805,7 @@ void sniff(u_char* arg, const struct pcap_pkthdr* hdr, const u_char* packet)
 	for(lp = attempts; lp; lp = lp->next) {
 		knocker_t *att = (knocker_t*)lp->data;
 		if(!strcmp(src_ip, att->src) &&
-		   !target_strcmp(dst_ip, att->door->target)) {
+		   ( strcmp(att->door->target, "any") == 0 || !target_strcmp(dst_ip, att->door->target) )) {
 			found_attempts = list_add(found_attempts, att);
 		}
 	}
